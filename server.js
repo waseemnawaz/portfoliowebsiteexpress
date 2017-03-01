a90aa036dfe1d2165bb92e3f240cd43cc5eb3893
waseemnawaz <waseemhijodenawaz@yahoo.es>
2017-03-01 09:39:52 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');// path to locate a folder
+var app = express();
+
+// Define the port to run on
+app.set('port', 3001);
+
+//app.use('/public', express.static(path.join(__dirname + '/public')));
+//connects the path to the static pages
+//when express receives a request for a route
+//it will see if the file path exists in the directory
+//matches routes that start with public
+app.use(express.static(path.join(__dirname + '/public')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
