0b0875d707f22362101b729f64536dcdc9cf96da
waseemnawaz <waseemhijodenawaz@yahoo.es>
2017-03-08 14:59:48 +0100

    added ejs and separated pages

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+
 var express = require('express');
 var path = require('path');// path to locate a folder
 var app = express();
+
+// Set view engine folder
+app.set('view engine', 'ejs')
+app.set('view cache', false);
 
 // Define the port to run on
 const port = process.env.PORT || 3001
@@ -11,7 +16,20 @@ app.set('port', port);
 //when express receives a request for a route
 //it will see if the file path exists in the directory
 //matches routes that start with public
-app.use(express.static(path.join(__dirname + '/public')));
+//app.use(express.static(path.join(__dirname + '/public')));
+app.use('/public', express.static('public'))
+
+app.get("/", function(req, res){
+    res.render("index.ejs", {});
+})
+
+app.get("/abt", function(req, res){
+    res.render("about.ejs");
+})
+
+app.get("/kntkto", function(req, res){
+    res.render("contact.ejs");
+})
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
